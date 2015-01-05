69d70d4949649701517fccf4f4940fc2d45ee7ec
Carlos Martinez <carlosjesus94@gmail.com>
2015-01-05 15:26:55 -0430

    as

--- app/app/App_Start/BundleConfig.cs
+++ app/app/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace app
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.1.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/vendor")
                    .Include("~/Scripts/vendor/angular.js", "~/Scripts/vendor/lodash.js", "~/Scripts/vendor/angular-google-maps.js")
